e6b6aeff7a23f40195ced7267a29f25a15ba62a3
德华 刘 <gavinprior5@gmail.com>
2024-09-23 12:58:23 +0000

    position links in navbar

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -6,16 +6,24 @@ import { Link } from "react-router-dom";
 
 function NavigationBar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
+    <Navbar expand="lg" className="" fixed="top">
       <Container>
         <Navbar.Brand href="#home">GoWithSoph</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/">Home</Link>
-            <Link to="/blog">Blog</Link>
-            <Link to="/register">Register</Link>
-            <Link to="/login">Login</Link>
+          <Nav className="ml-auto">
+            <Link to="/" className="pb-4 lg:mr-4">
+              Home
+            </Link>
+            <Link to="/blog" className="pb-4 lg:mr-4">
+              Blog
+            </Link>
+            <Link to="/register" className="pb-4 lg:mr-4">
+              Register
+            </Link>
+            <Link to="/login" className="pb-4 lg:mr-4">
+              Login
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
