8b1573e596d5a57ebf63e69bfa1742b275286558
德华 刘 <gavinprior5@gmail.com>
2024-09-23 12:08:19 +0000

    set up routes in router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,21 @@ import {
   Link,
   Outlet,
 } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Blog } from "./pages/Blog";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
 
 function App() {
   // * Router
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Root />}></Route>
-      // TODO create Root
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
     )
   );
 
