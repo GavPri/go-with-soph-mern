a87d1d517b41bd5f700743c7dbc7ebe86653bb55
德华 刘 <gavinprior5@gmail.com>
2024-09-23 12:01:56 +0000

    create router and root components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,33 @@
 import NavigationBar from "./components/NavigationBar";
-import {createBrowserRouter, createRoutesFromElements, Route, Link, Outlet} from 'react-router-dom'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <NavigationBar />
-    </>
+  // * Router
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}></Route>
+      // TODO create Root
+    )
   );
+
+  return <></>;
+
+  const Root = () => {
+    <>
+      <div>
+        <NavigationBar />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>;
+  };
 }
 
 export default App;
