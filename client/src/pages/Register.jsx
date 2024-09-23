95758b8bf0dcbe4a0e44c5eec28f45c09115396a
德华 刘 <gavinprior5@gmail.com>
2024-09-23 13:50:56 +0000

    create handle change for form inputs

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
 
 const Register = () => {
-
   const [data, setData] = useState({
-    name: '', 
-    email: '',
-    password: '',
-  })
+    name: "",
+    email: "",
+    password: "",
+  });
 
   const registerUser = (e) => {
     e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setData({
+      ...data,
+      [name]: value,
+    });
   };
   return (
     <div className="mt-12">
@@ -18,11 +26,26 @@ const Register = () => {
         onSubmit={registerUser}
       >
         <label htmlFor="name"></label>
-        <input type="text" placeholder="enter your name..." value={data.email} onChange={} />
+        <input
+          type="text"
+          placeholder="enter your name..."
+          value={data.email}
+          onChange={handleChange}
+        />
         <label htmlFor="email"></label>
-        <input type="text" placeholder="enter your email..." value={data.name} onChange={} />
+        <input
+          type="text"
+          placeholder="enter your email..."
+          value={data.name}
+          onChange={handleChange}
+        />
         <label htmlFor="password"></label>
-        <input type="text" placeholder="enter your password..." value={data.password} onChange={} />
+        <input
+          type="text"
+          placeholder="enter your password..."
+          value={data.password}
+          onChange={handleChange}
+        />
         <button type="submit">submit</button>
       </form>
     </div>
