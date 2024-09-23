409c854b416221ff3f83d9ddcf4683ce734bfc2f
德华 刘 <gavinprior5@gmail.com>
2024-09-23 13:37:52 +0000

    create boilerplate register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Register = () => {
-  return <div className="mt-12 bg-">Register</div>;
+  return (
+    <div className="mt-12 bg-">
+      <form className="flex flex-col justify-center items-center">
+        <label htmlFor="name"></label>
+        <input type="text" placeholder="enter your name..." />
+        <label htmlFor="email"></label>
+        <input type="text" placeholder="enter your email..." />
+        <label htmlFor="password"></label>
+        <input type="text" placeholder="enter your password..." />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
