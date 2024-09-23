bce2f7c278241d87d18862c968cd2b51c543372c
德华 刘 <gavinprior5@gmail.com>
2024-09-23 14:45:11 +0000

    create test route and controller

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 
 const app = express();
 
+app.use("/", require("./routes/authRoutes"));
+
 PORT = 8000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const test = require("../controllers/authControllers");
+
+router.use(
+  cors({
+    credentials: true,
+    origin: "https://5173-gavpri-gowithsophmern-74r3xp0o5o7.ws-eu116.gitpod.io",
+  })
+);
+
+router.get("/", test);
+
+module.exports = router;
