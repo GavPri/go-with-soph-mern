bf1386adf85fbd2ae101aa2ba0af8589ce8c3564
德华 刘 <gavinprior5@gmail.com>
2024-09-23 15:45:27 +0000

    create userSchema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,47 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "A usename is required"],
+    unique: [true, "This username is already taken"],
+    minlength: [3, "A username must be over 2 characters"],
+    maxlength: [50, "A username must be under 50 characters"],
+    match: [
+      /^[a-zA-Z0-9_-]+$/,
+      "Username can only contain letters, numbers, underscores, and hyphens",
+    ],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email is required"],
+    match: [/.+@.+\..+/, "Please enter a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters long"],
+  },
+  role: {
+    type: String,
+    enum: ["user", "author"],
+    default: "user",
+  },
+});
+
+userSchema.pre("save", function (next) {
+  const authorEmail = "author@example.coom";
+  if (this.email === authorEmail) {
+    this.role = "author";
+  } else {
+    this.role = "user";
+  }
+
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
