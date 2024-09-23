774181fcc9a53207b60d168a004b6606362be607
德华 刘 <gavinprior5@gmail.com>
2024-09-23 13:25:37 +0000

    add initial styling to navbar

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,27 +1,46 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { FaBars, FaTimes } from "react-icons/fa"; // Import icons
 import { Link } from "react-router-dom";
 
 function NavigationBar() {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <Navbar expand="lg" className="" fixed="top">
+    <Navbar expand="lg" className="bg-brand text-text" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">GoWithSoph</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Link to="/">
+          <h1>GoWithSoph</h1>
+        </Link>
+        <Navbar.Toggle
+          className="focus:outline-none focus:ring-0 border-0 text-text"
+          aria-controls="basic-navbar-nav"
+          onClick={() => setExpanded(expanded ? false : "expanded")}
+        >
+          {expanded ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Link to="/" className="pb-4 lg:mr-4">
+            <Link to="/" className="lg:mr-4 lg:flex lg:justify-center lg:pb-0">
               Home
             </Link>
-            <Link to="/blog" className="pb-4 lg:mr-4">
+            <Link
+              to="/blog"
+              className="lg:mr-4 lg:flex lg:justify-center lg:pb-0"
+            >
               Blog
             </Link>
-            <Link to="/register" className="pb-4 lg:mr-4">
+            <Link
+              to="/register"
+              className="lg:mr-4 lg:flex lg:justify-center lg:pb-0"
+            >
               Register
             </Link>
-            <Link to="/login" className="pb-4 lg:mr-4">
+            <Link
+              to="/login"
+              className="lg:mr-4 lg:flex lg:justify-center lg:pb-0"
+            >
               Login
             </Link>
           </Nav>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: "#014421", // Dark Green Base for background
+        text: "#F1F1F1", // White for text
+        border: "#00A86B", // Light Green for border
+        brand: "#00A86B", // Light Green for brand elements
+        error: "#E63946", // Bright Red for error messages
+        success: "#2ECC71", // Bright Green for success messages
+        accentPrimary: "#C7A600", // Muted Yellow for primary accent
+        accentSecondary: "#4C5C4B", // Greenish Gray for secondary accent
+      },
+    },
   },
   plugins: [],
 };
