12891a81f1acec5d54dd5df605460fb963ede819
德华 刘 <gavinprior5@gmail.com>
2024-09-23 13:42:57 +0000

    create loginUser to prevent page reload

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
 const Login = () => {
+  const loginUser = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="mt-12">
-      <form action="" className="flex flex-col justify-center items-center">
+      <form
+        action=""
+        className="flex flex-col justify-center items-center"
+        onSubmit={loginUser}
+      >
         <label htmlFor="email"></label>
         <input type="text" placeholder="enter your email..." />
         <label htmlFor="password"></label>
