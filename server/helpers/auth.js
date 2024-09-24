f656cf74489ddfdec1bfdf0f813d6ede73cd056c
德华 刘 <gavinprior5@gmail.com>
2024-09-24 14:58:50 +0000

    create hashpassword function to hash paswords

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -1,2 +1,17 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 
+const hashPassword = (password) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(12, (err, salt) => {
+      if (err) {
+        reject(err);
+      }
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(hash);
+      });
+    });
+  });
+};
