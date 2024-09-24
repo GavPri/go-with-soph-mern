39f0daa4e890b0677e2ecbeb48befb3f04195ff6
德华 刘 <gavinprior5@gmail.com>
2024-09-24 22:57:16 +0000

    add hero image to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="mt-20 min-h-[calc(100dvh-5rem)] bg-bg"></div>;
+  return (
+    <div className="mt-20 min-h-[calc(100dvh-5rem)] bg-bg w-screen">
+      {/* Image wrapper */}
+      <div className="w-full">
+        <img
+          className="bg-[url('./images/nevadaFalls.JPG')] h-[calc(100dvh-5rem)] w-full object-cover"
+          src="./images/nevadaFalls.JPG"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
