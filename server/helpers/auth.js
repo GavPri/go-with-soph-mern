448f022b2c587711573fd9497f217cb71d416f24
德华 刘 <gavinprior5@gmail.com>
2024-09-24 15:03:55 +0000

    export new functions from auth.js

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -17,5 +17,7 @@ const hashPassword = (password) => {
 };
 
 const comparePassword = (password, hashed) => {
-    return bcrypt.compare(password, hashed)
-}
+  return bcrypt.compare(password, hashed);
+};
+
+module.exports = { hashPassword, comparePassword };
