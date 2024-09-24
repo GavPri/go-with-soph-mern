b82c27525dc53fe33add7639a877702daacc6991
德华 刘 <gavinprior5@gmail.com>
2024-09-24 17:28:22 +0000

    refactor state, ref and useEffect

---
+++ client/src/hooks/useClickOutSideToggle.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useEffect, useRef, useState } from "react";
+
+const useClickOutSideToggle = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setExpanded(false);
+      }
+    };
+    document.addEventListener("mouseup", handleClickOutside);
+    return () => {
+      document.removeEventListener("mouseup", handleClickOutside);
+    };
+  }, [ref]);
+  
+  return {expanded, setExpanded, ref};
+};
+
+export default useClickOutSideToggle;
