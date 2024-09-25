73a95390c7a4ee481370466d7de76d57ca6ca9c3
德华 刘 <gavinprior5@gmail.com>
2024-09-25 16:49:20 +0000

    remove un used imports from navigation bar

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,15 +1,10 @@
-import { useEffect, useRef, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { FaBars, FaTimes } from "react-icons/fa"; // Import icons
 import { NavLink } from "react-router-dom";
 import useClickOutSideToggle from "../hooks/useClickOutSideToggle";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { IoMdSearch } from "react-icons/io";
+
 
 function NavigationBar() {
   const { expanded, setExpanded, ref } = useClickOutSideToggle();
