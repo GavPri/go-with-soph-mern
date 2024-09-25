e98be13b0c55e267d7bc0d328319d0466f1dbdf8
德华 刘 <gavinprior5@gmail.com>
2024-09-25 16:33:53 +0000

    rename & correct naming

--- client/src/context/userContext.jsx
+++ client/src/context/userContext.jsx
@@ -3,16 +3,22 @@ import { createContext, useState, useEffect } from "react";
 
 export const UserContext = createContext({});
 
-export function userContextProvider({ children }) {
+export function UserContextProvider({ children }) {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     if (!user) {
-      axios.get("/profile").then(({ data }) => {
-        setUser(data);
-      });
+      axios
+        .get("/profile")
+        .then(({ data }) => {
+          setUser(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching user profile:", error);
+        });
     }
-  }, []);
+  }, [user]); // Consider adding user to the dependency array if needed
+
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
