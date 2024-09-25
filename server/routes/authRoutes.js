3b422f76c83e0947f1a96e70484186c57002f3b2
德华 刘 <gavinprior5@gmail.com>
2024-09-25 17:57:00 +0000

    create post route in routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { test, registerUser, loginUser, getProfile, logoutUser } = require("../controllers/authControllers");
+const {
+  test,
+  registerUser,
+  loginUser,
+  getProfile,
+  logoutUser,
+} = require("../controllers/authControllers");
 
 router.get("/", test);
 router.post("/register", registerUser);
-router.post("/login", loginUser)
-router.get('/profile', getProfile)
+router.post("/login", loginUser);
+router.get("/profile", getProfile);
+router.post("logout", logoutUser);
 
 module.exports = router;
