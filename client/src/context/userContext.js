3a7a3eba8c6f0bc1b08ab28f03f5463fbba97f58
德华 刘 <gavinprior5@gmail.com>
2024-09-25 15:07:39 +0000

    create boilerplate useContextProvider

--- client/src/context/userContext.js
+++ client/src/context/userContext.js
@@ -1,4 +1,8 @@
 import axios from "axios";
-import {createContext} from 'react'
+import { createContext } from "react";
 
-export const userContext = createContext({})
+export const UserContext = createContext({});
+
+export function userContextProvider({ children }) {
+  return <UserContext.Provider value={<></>}>{children}</UserContext.Provider>;
+}
