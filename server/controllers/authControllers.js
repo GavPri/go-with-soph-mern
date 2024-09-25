67f8180f2c61722dd35eb7c72440a8c9580d80e9
德华 刘 <gavinprior5@gmail.com>
2024-09-25 15:39:17 +0000

    create get profile endpoint

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -116,8 +116,16 @@ const loginUser = async (req, res) => {
   }
 };
 
-const getProfile = () => {
-  
-}
+const getProfile = (req, res) => {
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, JWT_SECRET, {}, (err, user) => {
+      if (err) throw err;
+      res.json(user);
+    });
+  } else {
+    res.json(null);
+  }
+};
 
 module.exports = { test, registerUser, loginUser, getProfile };
