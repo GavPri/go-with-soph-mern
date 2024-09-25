7a9c9a5183824de16d3cbf07a316c929ad4d02d6
德华 刘 <gavinprior5@gmail.com>
2024-09-25 15:09:33 +0000

    add state and useEffect to context

--- client/src/context/userContext.js
+++ client/src/context/userContext.js
@@ -1,8 +1,11 @@
 import axios from "axios";
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const UserContext = createContext({});
 
 export function userContextProvider({ children }) {
+  const [user, setuser] = useState(null);
+
+  useEffect(() => {}, []);
   return <UserContext.Provider value={<></>}>{children}</UserContext.Provider>;
 }
