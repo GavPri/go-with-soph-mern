a06d89361158a8f771991d4c6f3b2c51f3d1e893
德华 刘 <gavinprior5@gmail.com>
2024-09-25 12:26:33 +0000

    add parser url settings

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const { mongoose } = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -17,6 +18,8 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", require("./routes/authRoutes"));
 
