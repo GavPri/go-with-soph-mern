8bf262742b93730478b2dd2a51ad182a3ad79b1c
德华 刘 <gavinprior5@gmail.com>
2024-09-26 12:48:31 +0000

    create fields for title, slug, author and heroImage

--- server/models/blogModel.js
+++ server/models/blogModel.js
@@ -1,4 +1,28 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const User = require("./userModel");
 
-const blogSchema = new Schema({});
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 100,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: User,
+    required: true,
+  },
+  heroImage: {
+    type: String,
+    required: true, 
+  }
+});
