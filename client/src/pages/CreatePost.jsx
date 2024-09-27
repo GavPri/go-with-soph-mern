151f2c5701c3262c5a8245b7ae8cdbbaee9b73e9
德华 刘 <gavinprior5@gmail.com>
2024-09-27 12:56:43 +0000

    create header for create blog

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { MdTravelExplore } from "react-icons/md";
 
 const CreatePost = () => {
   return (
-    <div className="mt-32 flex flex-col justify-center items-center w-full">
-      CreatePost
+    <div className="mt-28 flex flex-col justify-center items-center w-full font-qs text-text text-xl">
+      <div className="text-center flex flex-col justify-center items-center font-qs font-bold tracking-wider text-2xl mb-6 pt-6">
+        <h2 className="flex flex-col items-center text-brand text-xl mb-2">
+          {" "}
+          <MdTravelExplore size={40} /> GoWithSoph
+        </h2>
+        <p className="text-lg text-text ">Create a blog post</p>
+      </div>
     </div>
   );
 };
