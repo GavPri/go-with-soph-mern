be213f79f61689c49ca0d27b3612311bd03c1cfe
德华 刘 <gavinprior5@gmail.com>
2024-09-29 15:29:48 +0000

    add additional erorr handling.

--- server/controllers/blogControllers.js
+++ server/controllers/blogControllers.js
@@ -18,9 +18,12 @@ const createBlog = async (req, res) => {
     await newBlogPost.save();
     res.status(201).json({ message: "Blog post created successfully" });
   } catch (error) {
-    res
-      .status(400)
-      .json({ error: "There was an error creating the blog post." });
+    res;
+    console.error(error);
+    res.status(400).json({
+      error: "There was an error creating the blog post.",
+      details: error.message,
+    });
   }
 };
 
