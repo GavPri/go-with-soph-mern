1f24041abf40f82cdcf5ed3b5a86c2275fa8b884
德华 刘 <gavinprior5@gmail.com>
2024-09-29 14:25:48 +0000

    add module to ReactQuill

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -50,6 +50,31 @@ const CreatePost = () => {
     }));
   };
 
+  const toolbarOptions = [
+    ["bold", "italic", "underline", "strike"], // toggled buttons
+    ["blockquote", "code-block"],
+    ["link", "image", "video", "formula"],
+
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+    [{ script: "sub" }, { script: "super" }], // superscript/subscript
+    [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+    [{ direction: "rtl" }], // text direction
+
+    [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+
+    ["clean"], // remove formatting button
+  ];
+
+  const modules = {
+    toolbar: toolbarOptions,
+  };
+
   // todo handle submit function --- add it to form.
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -178,6 +203,7 @@ const CreatePost = () => {
         </select>
         {/* *Quill text editor */}
         <ReactQuill
+          modules={modules}
           value={formData.content}
           className="mb-6 rounded-md"
           onChange={() => {}}
