7f48df4ccdac905995d2359cc2ea92548dea4cf1
德华 刘 <gavinprior5@gmail.com>
2024-09-29 15:04:23 +0000

    create try block for createBlog

--- server/controllers/blogControllers.js
+++ server/controllers/blogControllers.js
@@ -4,4 +4,20 @@ const createBlog = async (req, res) => {
   const { title, slug, heroImage, tags, destination, continent, content } =
     req.body;
 
+  try {
+    const newBlogPost = new Blog({
+      title,
+      slug,
+      heroImage,
+      tags,
+      destination,
+      continent,
+      content,
+    });
+
+    await newBlogPost.save();
+    res.status(201).json({ message: "Blog post created successfully" });
+  } catch (error) {}
 };
+
+module.exports({ createBlog });
