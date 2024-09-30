e83e653f095a9f07047ae1038ef6aba66a6bcbd5
德华 刘 <gavinprior5@gmail.com>
2024-09-30 13:24:32 +0000

    add map function to display blog posts

--- client/src/pages/blogPages/Blog.jsx
+++ client/src/pages/blogPages/Blog.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import Card from "react-bootstrap/Card"; // Correct import for Card
+import { NavLink } from "react-router-dom"; // Correct import for NavLink
 
 const Blog = () => {
   const [blogs, setBlogs] = useState([]);
@@ -20,11 +21,29 @@ const Blog = () => {
 
   return (
     <div className="mt-28 flex flex-col justify-center items-center w-full font-qs text-text text-xl">
-      <ul>
-        {blogs.map((blog) => (
-          <></>
-        ))}
-      </ul>
+      <div>
+        {blogs.map((blog) => {
+          const { _id, title, heroImage, content } = blog; // Destructure id and content
+
+          return (
+            // Return the Card component
+            <div className="col-md-4 mb-4" key={_id}>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={heroImage} alt={title} />
+                <Card.Body>
+                  <Card.Title>{title}</Card.Title>
+                  <Card.Text>
+                    {content.length > 100
+                      ? `${content.slice(0, 100)}...`
+                      : content}{" "}
+                  </Card.Text>
+                  <NavLink>Read Blog</NavLink>{" "}
+                </Card.Body>
+              </Card>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
