50f8729e6fe0e9534b1e4bb37a55d5b4960af8c1
德华 刘 <gavinprior5@gmail.com>
2024-09-30 20:36:44 +0000

    add edit component and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import { Toaster } from "react-hot-toast";
 import { UserContextProvider } from "./context/userContext";
 import CreatePost from "./pages/blogPages/CreatePost";
 import BlogPost from "./pages/blogPages/BlogPost";
+import EditPost from "./pages/blogPages/EditPost";
 
 axios.defaults.baseURL = "https://gowithsoph.vercel.app";
 
@@ -45,6 +46,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/create" element={<CreatePost />} />
         <Route path="/blog/:_id" element={<BlogPost />} />
+        <Route path="/edit-blog/:_id" element={<EditPost />}
       </Route>
     )
   );
---
+++ client/src/pages/blogPages/EditPost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditPost = () => {
+  return (
+    <div className='mt-32'>EditPost</div>
+  )
+}
+
+export default EditPost
