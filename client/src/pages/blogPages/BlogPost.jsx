1c0354613b6a0a533d029f31ea40f3c9073f936f
德华 刘 <gavinprior5@gmail.com>
2024-09-30 14:57:20 +0000

    add fetch to useffect

--- client/src/pages/blogPages/BlogPost.jsx
+++ client/src/pages/blogPages/BlogPost.jsx
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const BlogPost = () => {
   const { id } = useParams();
   const [blogData, setBlogData] = useState(null);
+
+  useEffect(() => {
+    const fetchBlog = async () => {
+      try {
+        const response = await axios.get("/get-blogs");
+        setBlogData(response.data);
+        console.log(setBlogData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchBlog();
+  }, []);
+
   return <div className="mt-32">BlogPost</div>;
 };
 
