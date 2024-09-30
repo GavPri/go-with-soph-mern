0a4b5a7b6e90f21fe8ebc560a3456cc83e9853f5
德华 刘 <gavinprior5@gmail.com>
2024-09-30 14:51:22 +0000

    add useParams and state to post

--- client/src/pages/blogPages/BlogPost.jsx
+++ client/src/pages/blogPages/BlogPost.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const BlogPost = () => {
+  const { id } = useParams();
+  const [blogData, setBlogData] = useState(null);
   return <div className="mt-32">BlogPost</div>;
 };
 
