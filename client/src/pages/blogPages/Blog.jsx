16d84568d981403404921a6d7d527b116d1e02cf
德华 刘 <gavinprior5@gmail.com>
2024-09-30 12:56:46 +0000

    add request and useEffect to test blog

--- client/src/pages/blogPages/Blog.jsx
+++ client/src/pages/blogPages/Blog.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from 'axios'
 
 const Blog = () => {
+  const getBlogs = async () => {
+    const response = await axios.get("/get-blogs");
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getBlogs();
+  }, []);
+
   return (
-    <div className="mt-28 flex flex-col justify-center items-center w-full font-qs text-text text-xl">
-    </div>
+    <div className="mt-28 flex flex-col justify-center items-center w-full font-qs text-text text-xl"></div>
   );
 };
 
