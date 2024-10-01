6ec0c20d5584917f3f26c79610baa6d36dbdea1e
德华 刘 <gavinprior5@gmail.com>
2024-10-01 15:54:58 +0000

    adjust router for search

--- server/routes/searchRoutes.js
+++ server/routes/searchRoutes.js
@@ -2,4 +2,9 @@ const express = require("express");
 
 const router = express.Router()
 
-const { searchBlogs} = require('')
+const { searchBlogs } = require('../controllers/searchController');
+
+
+router.get('/search-blogs', searchBlogs)
+
+module.exports = router
