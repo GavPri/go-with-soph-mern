4e0c74358eacbb144bfe8131439371e1e9c4a762
德华 刘 <gavinprior5@gmail.com>
2024-10-01 16:13:01 +0000

    add serach bar for modal

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,10 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const SearchBar = () => {
-    const [searchTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div>SearchBar</div>
-  )
-}
+    <div className="font-qs">
+      <form>
+        <label for="search" class="mb-2 text-sm font-medium text-text sr-only">
+          Search
+        </label>
+        <div class="relative">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              class="w-4 h-4 text-text-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="search"
+            class="block w-full p-4 ps-10 text-sm text-text border border-border rounded-lg focus:ring-accentPrimary focus:border-accentPrimary "
+            placeholder="Search"
+            required
+          />
+          <button
+            type="submit"
+            class="text-white absolute end-2.5 bottom-2.5 bg-brand hover:bg-brand focus:ring-4 focus:outline-none focus:ring-border font-medium rounded-lg text-sm px-4 py-2"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
