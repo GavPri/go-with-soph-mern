1927e3ed2b45bf51ac968ed8118d492092aea38e
德华 刘 <gavinprior5@gmail.com>
2024-10-01 11:07:55 +0000

    add controller to delete blog post

--- server/controllers/blogControllers.js
+++ server/controllers/blogControllers.js
@@ -115,16 +115,27 @@ const editBlogsID = async (req, res) => {
 
 const deleteBlogsID = async (req, res) => {
   const { _id } = req.params;
-  const {
-    title,
-    heroImage,
-    slug,
-    tags,
-    destination,
-    continent,
-    content,
-    author,
-  } = req.body;
+
+  if (!id) {
+    res.status(404).json({ error: "No ID found" });
+  }
+
+  try {
+    const blogToDelete = await Blog.findByIdAndDelete(_id);
+    if (!blogToDelete) {
+      res.status(404).json({ error: "No blog was found." });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Blog post was deleted" }, { blogPost: blogToDelete });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      error: "There was an error updating the blog post.",
+      details: error.message,
+    });
+  }
 };
 
 module.exports = {
