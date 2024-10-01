f00d2a8c7b734521a133ef5e66a7adfdc5b56de8
德华 刘 <gavinprior5@gmail.com>
2024-10-01 15:47:26 +0000

    create search route

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,5 +1,23 @@
 const Blog = require("../models/blogModel");
 
-const searchBlogs = async (req, res) => {};
+const searchBlogs = async (req, res) => {
+  try {
+    const query = req.query.q;
+
+    const searchResults = await Blog.find({
+      $or: [
+        { title: { $regex: query, options: "i" } },
+        { content: { $regex: query, options: "i" } },
+        { tags: { $regex: query, options: "i" } },
+        { destination: { $regex: query, options: "i" } },
+        { continent: { $regex: query, options: "i" } },
+      ],
+    });
+
+    res.json(searchResults);
+  } catch (error) {
+    res.status(404).json({error: 'No blog post found'})
+  }
+};
 
 module.exports = { searchBlogs };
