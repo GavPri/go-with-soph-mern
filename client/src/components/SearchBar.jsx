573485ce90e8c07e05e49d0a892a7ff5f94168c1
德华 刘 <gavinprior5@gmail.com>
2024-10-03 10:23:18 +0000

    adjust styles of input field

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -49,7 +49,7 @@ const SearchBar = () => {
           <input
             type="search"
             id="search"
-            class="block w-8/12 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            class="block w-8/12 p-4 ps-10 text-sm border-border border-2 drop-shadow-md text-text"
             placeholder="Search"
             required
             onChange={(e) => {
