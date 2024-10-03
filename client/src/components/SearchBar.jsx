17ee18620e8972dd5f9c2a7e8df5db54a96a79db
德华 刘 <gavinprior5@gmail.com>
2024-10-03 10:19:12 +0000

    add prelim css to new form

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -33,7 +33,7 @@ const SearchBar = () => {
     // Form Container
     <div className="w-75 mb-3">
       {/* Form */}
-      <form>
+      <form className="w-full">
         <label
           for="search"
           class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
@@ -41,14 +41,14 @@ const SearchBar = () => {
           Search
         </label>
         {/* Input wrapper */}
-        <div class="relative">
+        <div class="flex items-center w-full bg-bg">
           {/* Icon wrapper */}
-          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"></div>
+          <div class="w-2/12 mr-2"></div>
           {/* Input */}
           <input
             type="search"
             id="search"
-            class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            class="block w-8/12 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Search"
             required
             onChange={(e) => {
