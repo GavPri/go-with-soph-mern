f5a98cb76febc01b81f3eb0aa424a1b9747fb730
德华 刘 <gavinprior5@gmail.com>
2024-10-04 10:13:43 +0000

    add try catch block

--- server/controllers/likeBlogController.js
+++ server/controllers/likeBlogController.js
@@ -1,21 +1,28 @@
 const blog = require("../models/blogModel");
 
 const likeBlogPost = async (req, res) => {
-  const blogID = req.params._id; // Blog id is stored in the url
-  const userID = req.user._d; // User id authenticated.
+  try {
+    const blogID = req.params._id; // Blog id is stored in the url
+    const userID = req.user._d; // User id authenticated.
 
-  const blogPost = await blog.findById(blogID); // Await the request to match blog posts.
+    const blogPost = await blog.findById(blogID); // Await the request to match blog posts.
 
-  if (!blogPost) {
-    return res.status(401).json({ error: "No blog post found." }); // Check if the block post exists
+    if (!blogPost) {
+      return res.status(401).json({ error: "No blog post found." }); // Check if the block post exists
+    }
+
+    if (blogPost.likeCount.includes(userID)) {
+      return res.status(400).json({ message: "You have already liked post." }); // Check if user has already liked the post
+    }
+
+    blogPost.likeCount.push(userID); // add users likes to the array & save
+    await blogPost.save();
+
+    res.status(200).json({ message: "Liked blog post." });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({error: 'Failed to like blog post.'})
   }
-
-  if (blogPost.likeCount.includes(userID)) {
-    return res.status(400).json({ message: "You have already liked post." }); // Check if user has already liked the post
-  }
-
-  blogPost.likeCount.push(userID); // add users likes to the array & save
-  await blogPost.save()
 };
 
 module.exports = { likeBlogPost };
