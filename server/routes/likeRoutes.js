895aef8111e41de8013e7df365811ae3a264537a
德华 刘 <gavinprior5@gmail.com>
2024-10-04 10:34:08 +0000

    add post route and export

--- server/routes/likeRoutes.js
+++ server/routes/likeRoutes.js
@@ -5,3 +5,7 @@ const router = express.Router();
 const { likeblogPost } = require("../controllers/likeBlogController");
 
 const { authenticate } = require("../middleware/authenticateUser");
+
+router.post('/:id/likes', authenticate, likeblogPost)
+
+module.exports = {router}
