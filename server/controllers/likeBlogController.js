b956d7d39fadf507abecbc49406e75c03eebcf72
德华 刘 <gavinprior5@gmail.com>
2024-10-04 09:57:47 +0000

    create blogPost variable

--- server/controllers/likeBlogController.js
+++ server/controllers/likeBlogController.js
@@ -3,6 +3,8 @@ const blog = require("../models/blogModel");
 const likeBlogPost = async (req, res) => {
   const blogID = req.params._id; // Blog id is stored in the url
   const userID = req.user._d; // User id authenticated.
+
+  const blogPost = await blog.findById(blogID) // Await the request to match blog posts.
 };
 
 module.exports = { likeBlogPost };
