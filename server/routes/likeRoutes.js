6952c6cdbda33f67a1423d7015e8f774e143c5a7
德华 刘 <gavinprior5@gmail.com>
2024-10-04 11:01:42 +0000

    adjust imports tolike routes

--- server/routes/likeRoutes.js
+++ server/routes/likeRoutes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 const { likeBlogPost } = require("../controllers/likeBlogController");
 
-const authenticateUser = require("../middleware/authenticateUser");
+const { authenticateUser } = require("../middleware/authenticateUser");
 
-router.post("/:_id/likes", authenticateUser, likeblogPost);
+router.post("/:_id/likes", authenticateUser, likeBlogPost);
 
 module.exports = router;
