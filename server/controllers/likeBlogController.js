e0303eee1abd4638afaaf9f2e76f8db80f763a7e
德华 刘 <gavinprior5@gmail.com>
2024-10-04 10:00:23 +0000

    add check for blog post

--- server/controllers/likeBlogController.js
+++ server/controllers/likeBlogController.js
@@ -4,7 +4,11 @@ const likeBlogPost = async (req, res) => {
   const blogID = req.params._id; // Blog id is stored in the url
   const userID = req.user._d; // User id authenticated.
 
-  const blogPost = await blog.findById(blogID) // Await the request to match blog posts.
+  const blogPost = await blog.findById(blogID); // Await the request to match blog posts.
+
+  if (!blogPost) {
+    return res.status(401).json({ error: "No blog post found." }); // Check if the block post exists
+  }
 };
 
 module.exports = { likeBlogPost };
