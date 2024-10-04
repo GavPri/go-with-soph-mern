e79632bd100e38380b1c24fda2366742866355e3
德华 刘 <gavinprior5@gmail.com>
2024-10-04 09:25:55 +0000

    function and export
    add skeleton of function and export function

--- server/middleware/authenticateUser.js
+++ server/middleware/authenticateUser.js
@@ -2,3 +2,9 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const JWT_SECRET = process.env.JWT_SECRET;
+
+// * Function to authenticate action
+
+const authenticateUser = async (req, res, next) => {};
+
+module.exports = { authenticateUser };
