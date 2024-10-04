32dd98fc6a6aa15cd31d6ed9ea73bb6a3feee563
德华 刘 <gavinprior5@gmail.com>
2024-10-04 09:34:03 +0000

    add token variable

--- server/middleware/authenticateUser.js
+++ server/middleware/authenticateUser.js
@@ -5,6 +5,10 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 // * Function to authenticate action
 
-const authenticateUser = async (req, res, next) => {};
+const authenticateUser = async (req, res, next) => {
+  // * Get cookies from authorisation header
+  const token =
+    req.cookies.token || req.Header("Authorization")?.replace("Bearer", "");
+};
 
 module.exports = { authenticateUser };
