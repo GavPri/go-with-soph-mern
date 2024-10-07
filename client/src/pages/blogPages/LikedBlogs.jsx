c38034b5f9db72222fb7cad2a2b4dbd8270355c1
德华 刘 <gavinprior5@gmail.com>
2024-10-07 15:09:11 +0000

    adjust style of likedBlogs

--- client/src/pages/blogPages/LikedBlogs.jsx
+++ client/src/pages/blogPages/LikedBlogs.jsx
@@ -4,6 +4,7 @@ import { NavLink } from "react-router-dom";
 import { Card } from "react-bootstrap"; //
 import { MdLocationPin } from "react-icons/md";
 import axios from "axios";
+import SearchBar from "../../components/SearchBar";
 
 const LikedBlogs = () => {
   const [likedBlogs, setLikedBlogs] = useState([]); // state for storing blog posts.
@@ -28,8 +29,10 @@ const LikedBlogs = () => {
   }, [user]);
 
   return (
-    <div className="liked-posts">
+    <div className="mt-28 flex flex-col justify-center items-center w-full font-qs text-text text-xl">
+      <SearchBar />
       <h1>Your Liked Posts</h1>
+      <div></div>
       {!user ? (
         <p>Please log in to view your liked posts.</p>
       ) : likedBlogs.length > 0 ? (
@@ -38,38 +41,43 @@ const LikedBlogs = () => {
           const { _id, title, heroImage, tags, destination } = post;
 
           return (
-            <Card key={_id} className="h-100 text-text bg-bg">
-              <Card.Img
-                variant="top"
-                src={heroImage}
-                alt={title}
-                className="object-cover h-[250px]"
-              />
-              <Card.Body className="d-flex flex-column">
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>
-                  <div className="d-flex flex-wrap mb-2">
-                    {tags.map((tag, index) => (
-                      <div
-                        key={index}
-                        className="rounded bg-light border text-brand px-2 py-1 mr-2 mb-2"
-                      >
-                        {tag}
-                      </div>
-                    ))}
+            <div className="col-md-4 mb-4 w-3/4" key={_id} >
+              <Card key={_id} className="h-100 text-text bg-bg w-3/12">
+                <Card.Img
+                  variant="top"
+                  src={heroImage}
+                  alt={title}
+                  className="object-cover h-[250px]"
+                />
+                <Card.Body className="d-flex flex-column">
+                  <Card.Title>{title}</Card.Title>
+                  <Card.Text>
+                    <div className="d-flex flex-wrap mb-2">
+                      {tags.map((tag, index) => (
+                        <div
+                          key={index}
+                          className="rounded bg-light border text-brand px-2 py-1 mr-2 mb-2"
+                        >
+                          {tag}
+                        </div>
+                      ))}
+                    </div>
+                    <div className="d-flex align-items-center mb-2">
+                      <MdLocationPin size={20} />{" "}
+                      <span className="ml-1">{destination}</span>
+                    </div>
+                  </Card.Text>
+                  <div className="mt-auto">
+                    <NavLink
+                      className="btn bg-brand text-bg"
+                      to={`/blog/${_id}`}
+                    >
+                      Read Blog
+                    </NavLink>
                   </div>
-                  <div className="d-flex align-items-center mb-2">
-                    <MdLocationPin size={20} />{" "}
-                    <span className="ml-1">{destination}</span>
-                  </div>
-                </Card.Text>
-                <div className="mt-auto">
-                  <NavLink className="btn bg-brand text-bg" to={`/blog/${_id}`}>
-                    Read Blog
-                  </NavLink>
-                </div>
-              </Card.Body>
-            </Card>
+                </Card.Body>
+              </Card>
+            </div>
           );
         })
       ) : (
