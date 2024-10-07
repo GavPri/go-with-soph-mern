5ee1ef9ecb617860c79ffa27673f25771e32bdbb
德华 刘 <gavinprior5@gmail.com>
2024-10-07 14:09:59 +0000

    import useState

--- client/src/pages/blogPages/LikedBlogs.jsx
+++ client/src/pages/blogPages/LikedBlogs.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LikedBlogs = () => {
-  const [likedBlogs, setLikedBlogs] = useState([]); // state for storing blog posts. 
+  const [likedBlogs, setLikedBlogs] = useState([]); // state for storing blog posts.
   return <div>LikedBlogs</div>;
 };
 
