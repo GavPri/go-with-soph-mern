dced4cddddb51a7ef0b7de35d319d026fd52a55b
德华 刘 <gavinprior5@gmail.com>
2024-10-07 15:34:08 +0000

    configure routes for unlike fn

--- server/routes/likeRoutes.js
+++ server/routes/likeRoutes.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const {
   likeBlogPost,
   getLikedPosts,
+  unlikeBlogPost,
 } = require("../controllers/likeBlogController");
 
 const { authenticateUser } = require("../middleware/authenticateUser");
 
 router.post("/blogs/:_id/likes", authenticateUser, likeBlogPost);
 router.get("/liked-blogs", authenticateUser, getLikedPosts);
+router.post("blogs/:_id/unlike", authenticateUser, unlikeBlogPost);
 
 module.exports = router;
