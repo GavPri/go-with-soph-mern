aa796f6d9dc702f86176be28de558136001a0b68
德华 刘 <gavinprior5@gmail.com>
2024-10-07 14:11:04 +0000

    add boilerplate useEffect

--- client/src/pages/blogPages/LikedBlogs.jsx
+++ client/src/pages/blogPages/LikedBlogs.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const LikedBlogs = () => {
   const [likedBlogs, setLikedBlogs] = useState([]); // state for storing blog posts.
+
+  useEffect(() => {
+    const fetchLikedPosts = async () => {
+      try {
+      } catch (error) {}
+    };
+  });
   return <div>LikedBlogs</div>;
 };
 
