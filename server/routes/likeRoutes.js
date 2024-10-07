a7396538a83c385f71183b93ee18479d8535f05f
德华 刘 <gavinprior5@gmail.com>
2024-10-07 14:05:38 +0000

    add getLikedPosts to router

--- server/routes/likeRoutes.js
+++ server/routes/likeRoutes.js
@@ -2,10 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-const { likeBlogPost } = require("../controllers/likeBlogController");
+const {
+  likeBlogPost,
+  getLikedPosts,
+} = require("../controllers/likeBlogController");
 
 const { authenticateUser } = require("../middleware/authenticateUser");
 
 router.post("/blogs/:_id/likes", authenticateUser, likeBlogPost);
+router.get("/liked-blogs", authenticateUser, getLikedPosts);
 
 module.exports = router;
