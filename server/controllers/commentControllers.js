d8a4e3268d4013621ccc210657a1ca11c69f008f
德华 刘 <gavinprior5@gmail.com>
2024-10-08 12:51:17 +0000

    add variable to the comment controller

--- server/controllers/commentControllers.js
+++ server/controllers/commentControllers.js
@@ -1,3 +1,9 @@
-const createComment = async (req, res) => {};
+const createComment = async (req, res) => {
+  const blogID = req.params._id; // get the blog id from the url
+  const userID = req.user._id; // get the user id from auth.
+  const content = req.body; // get the comment content
+
+  
+};
 
 module.exports = { createComment };
