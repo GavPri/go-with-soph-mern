4421b30567906e42049573ff2d44cc89321f4cc1
德华 刘 <gavinprior5@gmail.com>
2024-10-08 12:55:04 +0000

    add await and trycatch to comment controller

--- server/controllers/commentControllers.js
+++ server/controllers/commentControllers.js
@@ -1,9 +1,15 @@
+const Blog = require("../models/blogModel");
+const User = require("../models/userModel");
+
 const createComment = async (req, res) => {
   const blogID = req.params._id; // get the blog id from the url
   const userID = req.user._id; // get the user id from auth.
   const content = req.body; // get the comment content
 
-  
+  const blog = await Blog.findById(blogID); // await the blog post being found.
+
+  try {
+  } catch (error) {}
 };
 
 module.exports = { createComment };
