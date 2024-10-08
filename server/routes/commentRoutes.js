e96557f9e5714b65a49cf376881cea15ae182777
德华 刘 <gavinprior5@gmail.com>
2024-10-08 12:39:55 +0000

    create intial comment route

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -1,1 +1,9 @@
-const express = require('express');
+const express = require("express");
+
+const router = express.Router();
+
+const { createComment } = require("../controllers/commentContorllers");
+
+router.post(":_id/comment", createComment);
+
+module.exports = router;
