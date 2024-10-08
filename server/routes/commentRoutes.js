5cc00512b47abae55157fe5da47e737c1e9ed77c
德华 刘 <gavinprior5@gmail.com>
2024-10-08 15:33:09 +0000

    add authenticate user to comment

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -3,7 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 const { createComment } = require("../controllers/commentControllers");
+const { authenticateUser } = require("../middleware/authenticateUser");
 
-router.post("/blogs/:_id/comment", createComment);
+router.post("/blogs/:_id/comment", authenticateUser, createComment);
 
 module.exports = router;
