a56441348cc343b6f32732d0a333bbf5e7f2546d
德华 刘 <gavinprior5@gmail.com>
2024-10-08 19:05:34 +0000

    add route to delete

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -2,9 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-const { createComment } = require("../controllers/commentControllers");
+const {
+  createComment,
+  deleteComment,
+} = require("../controllers/commentControllers");
 const { authenticateUser } = require("../middleware/authenticateUser");
 
 router.post("/blogs/:_id/comment", authenticateUser, createComment);
+router.delete("/blogs/:_id/delete", authenticateUser, deleteComment);
 
 module.exports = router;
