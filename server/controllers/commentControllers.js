a4284c521d8d9d11473812746b89d9a68744c6e1
德华 刘 <gavinprior5@gmail.com>
2024-10-08 16:34:19 +0000

    adjust comment controller

--- server/controllers/commentControllers.js
+++ server/controllers/commentControllers.js
@@ -1,52 +1,54 @@
 const Blog = require("../models/blogModel");
-const User = require("../models/userModel");
+const User = require("../models/userModel"); // Ensure User model is imported if needed
 
 const createComment = async (req, res) => {
-  const blogID = req.params._id; // get the blog id from the url
-  const userID = req.user._id; // get the user id from auth.
-  const content = req.body.content; // get the comment content
-
-  const blog = await Blog.findById(blogID); // await the blog post being found.
-
   try {
+    const blogID = req.params._id; 
+    const userID = req.user._id; 
+    const content = req.body.content; 
+
+    if (!userID){
+      return res.status(500).json({error: 'No user id found.'})
+    }
+  
+    const blog = await Blog.findById(blogID);
     if (!blog) {
       return res.status(404).json({ error: "No blog found." });
     }
-    if (content.length < 1 || content.length > 1000) {
+
+   
+    if (!content || content.length < 1 || content.length > 1000) {
       return res
         .status(400)
         .json({ error: "Comment must be between 1 and 1000 characters." });
     }
 
-    const comment = new Comment({
-      user: userID,
-      blog: blogID,
+    const comment = {
+      user: userID, 
       content,
-    }); // create new comment with the fields required for the schema
+      createdAt: new Date(), 
+    };
 
-    await comment.save(); // await the comment to be saved.
+    blog.comments.push(comment);
 
-    await Blog.findByIdAndUpdate(blogID, {
-      $push: { comments: comment._id }, // update the blog to populate comments.
-    });
+    
+    await blog.save();
 
-    await comment.populate("user", "username")
-
+  
     res.status(201).json({
       message: "Comment created successfully.",
       comment: {
-        _id: comment._id,
-        blog: comment.blog,
         user: {
-          _id: comment.user,
-          username: comment.user.username,
+          _id: userID,
+          username: req.user.username,
         },
         content: comment.content,
       },
+      blog: blog, 
     });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Catch block error." });
+    console.error(error); 
+    res.status(500).json({ error: "Internal server error." });
   }
 };
 
