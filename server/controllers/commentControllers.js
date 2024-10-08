eba92e191887c005b755c9158ce6ae982fdac27e
德华 刘 <gavinprior5@gmail.com>
2024-10-08 13:20:18 +0000

    add user to comment controller
    required field in the schema

--- server/controllers/commentControllers.js
+++ server/controllers/commentControllers.js
@@ -4,6 +4,7 @@ const User = require("../models/userModel");
 
 const createComment = async (req, res) => {
   const blogID = req.params._id; // get the blog id from the url
+  const userID = req.user._id; // get the user id from auth.
   const content = req.body.content; // get the comment content
 
   const blog = await Blog.findById(blogID); // await the blog post being found.
