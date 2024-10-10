9c504a27e2770ef438281396ac7776038116deae
德华 刘 <gavinprior5@gmail.com>
2024-10-10 14:05:23 +0000

    add request to fetch new and like dblogs

--- client/src/components/BlogListings.jsx
+++ client/src/components/BlogListings.jsx
@@ -1,12 +1,27 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const BlogListings = () => {
   const [newestBlogs, setNewestBlogs] = useState([]);
   const [mostLikedBlogs, setMostLikedBlogs] = useState([]);
 
+  const getBlogListings = async () => {
+    const { data: newestData } = await axios.get(
+      "/get-blogs?page=1&limit=3&sortBy=newest"
+    );
+    setNewestBlogs(newestData);
+
+    const { data: LikesData } = await axios.get(
+      "/get-blogs?page=1&limit=3&sortBy=newest"
+    );
+    setMostLikedBlogs(LikesData);
+  };
+  
   useEffect(() => {
-    getBlogListings() // todo create getBlogListings Fn
-  },[])
+    getBlogListings(); // todo create getBlogListings Fn
+  }, []);
+
+
   return <div>BlogListings</div>;
 };
 
