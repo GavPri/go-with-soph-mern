fc31f40c3a6b64d0211dc82e2fa4633a884c922e
德华 刘 <gavinprior5@gmail.com>
2024-10-10 14:50:52 +0000

    add BlogListings

--- client/src/pages/blogPages/Blog.jsx
+++ client/src/pages/blogPages/Blog.jsx
@@ -1,34 +1,12 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Card from "react-bootstrap/Card";
-import { NavLink } from "react-router-dom";
-import DOMpurify from "dompurify";
-import { MdLocationPin } from "react-icons/md";
+import React from "react";
 import SearchBar from "../../components/SearchBar";
-import BlogRow from "../../components/BlogRow";
+import BlogListings from "../../components/BlogListings";
 
 const Blog = () => {
-  const [blogs, setBlogs] = useState([]);
-
-  const getBlogs = async () => {
-    try {
-      const { data } = await axios.get(
-        "/get-blogs?page=1&limit=3&sortBy=newest"
-      );
-      setBlogs(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getBlogs();
-  }, []);
-
   return (
     <div className="mt-28 flex flex-col justify-center items-center w-full font-qs text-text text-xl">
       <SearchBar />
-      <BlogRow blogs={blogs} />
+      <BlogListings />
     </div>
   );
 };
