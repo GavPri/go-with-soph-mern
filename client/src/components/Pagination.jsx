5733337b0b3ee02b1789cc7db95fae4505eea4c8
德华 刘 <gavinprior5@gmail.com>
2024-10-10 18:18:36 +0000

    add styles to buttons

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Pagination = () => {
   return (
-    <div>Pagination</div>
-  )
-}
+    <div className="flex justify-between items-center gap-2">
+      <button className="rounded-md border-border font-qs bg-bg text-accentSecondary p-4">
+        Previous Post
+      </button>
+      <button className="rounded-md border-border font-qs bg-bg text-accentSecondary p-4">
+        Next Post
+      </button>
+    </div>
+  );
+};
 
-export default Pagination
+export default Pagination;
