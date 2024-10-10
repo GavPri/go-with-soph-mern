175bcd49dffd81fdf6a1368853150ef92d775bd3
德华 刘 <gavinprior5@gmail.com>
2024-10-10 18:56:07 +0000

    add return for total pages

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Pagination = () => {
   return (
     <div className="flex justify-between items-center gap-2">
-      <button className="rounded-md border-border font-qs bg-bg text-accentSecondary p-4">
+      <button className="rounded-md font-qs bg-bg text-accentSecondary p-4 hover:bg-brand hover:text-bg transition-all duration-300 ease-in-out">
         Previous Post
       </button>
-      <button className="rounded-md border-border font-qs bg-bg text-accentSecondary p-4">
+      <button className="rounded-md font-qs bg-bg text-accentSecondary p-4 hover:bg-brand hover:text-bg transition-all duration-300 ease-in-out">
         Next Post
       </button>
     </div>
