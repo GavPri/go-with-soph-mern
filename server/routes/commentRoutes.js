26ddd320e91eaec604fc8c4cb15658f0b7eae2c3
德华 刘 <gavinprior5@gmail.com>
2024-10-10 10:19:58 +0000

    add edit routes to commentRoutes

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const {
   createComment,
   deleteComment,
+  editComment
 } = require("../controllers/commentControllers");
 const { authenticateUser } = require("../middleware/authenticateUser");
 
 router.post("/blogs/:_id/comment", authenticateUser, createComment);
 router.delete("/blogs/:_id/delete/:commentId", authenticateUser, deleteComment);
+router.put("/blogs/:_id/edit/:commentId", authenticateUser, editComment)
 
 module.exports = router;
