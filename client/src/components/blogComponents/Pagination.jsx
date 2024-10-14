ccd84df1b9b7bf9fdfc79e18e7beb31ac97cc252
德华 刘 <gavinprior5@gmail.com>
2024-10-14 12:56:03 +0000

    add disabled styling

--- client/src/components/blogComponents/Pagination.jsx
+++ client/src/components/blogComponents/Pagination.jsx
@@ -6,14 +6,21 @@ const Pagination = ({ currentPage, totalPages, onNext, onPrevious }) => {
       <button
         disabled={currentPage === 1}
         onClick={onPrevious}
-        className="rounded-md font-qs bg-bg text-accentSecondary p-4 hover:bg-brand hover:text-bg transition-all duration-300 ease-in-out"
+        className={`rounded-md font-qs bg-bg text-accentSecondary p-4 hover:bg-brand hover:text-bg transition-all duration-300 ease-in-out ${
+          currentPage === 1 ? "hidden" : ""
+        }`}
       >
         Previous Post
       </button>
+      <p className="font-qs text-brand font-bold">
+        {currentPage} / <span className="text-text">{totalPages}</span>
+      </p>
       <button
         disabled={currentPage === totalPages}
         onClick={onNext}
-        className="rounded-md font-qs bg-bg text-accentSecondary p-4 hover:bg-brand hover:text-bg transition-all duration-300 ease-in-out"
+        className={`rounded-md font-qs bg-bg text-accentSecondary p-4 hover:bg-brand hover:text-bg transition-all duration-300 ease-in-out ${
+          currentPage === totalPages ? "hidden" : ""
+        }`}
       >
         Next Post
       </button>
