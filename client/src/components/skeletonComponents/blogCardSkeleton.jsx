80155385a24ba103969a618a823bd65024091bce
德华 刘 <gavinprior5@gmail.com>
2024-10-14 12:01:37 +0000

    adjust style of skeleton

--- client/src/components/skeletonComponents/blogCardSkeleton.jsx
+++ client/src/components/skeletonComponents/blogCardSkeleton.jsx
@@ -5,22 +5,32 @@ import Card from "react-bootstrap/Card";
 
 const BlogCardSkeleton = () => {
   return (
-    <>
-      <Card style={{ width: "18rem" }}>
-        <Skeleton height={320} />
-        <Card.Body>
+    <div className="col-md-4 mb-4">
+      <Card className="h-100 text-text bg-bg">
+        <Skeleton height={250} />
+        <Card.Body className="d-flex flex-column">
           <Card.Title>
             <Skeleton />
           </Card.Title>
           <Card.Text>
-            <Skeleton count={3} />
+            <div className="d-flex flex-wrap mb-2">
+              <div className="rounded bg-light border text-brand px-2 py-1 mr-2 mb-2">
+                <Skeleton width={60} count={3} />
+              </div>
+            </div>
+            <div className="d-flex align-items-center mb-2">
+              <Skeleton width={50} />
+              <span className="ml-1">
+                <Skeleton />
+              </span>
+            </div>
           </Card.Text>
-          <p>
-            <Skeleton width={50} />
-          </p>
+          <div className="mt-auto">
+              <Skeleton width={50}/>
+          </div>
         </Card.Body>
       </Card>
-    </>
+    </div>
   );
 };
 
