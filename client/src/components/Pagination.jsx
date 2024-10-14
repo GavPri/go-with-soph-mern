32d6df493b3d742f3c06a3c3ab256412dc5e421b
德华 刘 <gavinprior5@gmail.com>
2024-10-14 08:36:10 +0000

    add props to button

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
-const Pagination = () => {
+const Pagination = ({ currentPage, totalPages, onNext, onPrevious }) => {
   return (
     <div className="flex justify-between items-center gap-2">
-      <button className="rounded-md font-qs bg-bg text-accentSecondary p-4 hover:bg-brand hover:text-bg transition-all duration-300 ease-in-out">
+      <button
+        disabled={currentPage === 1}
+        onPrevious={onPrevious}
+        className="rounded-md font-qs bg-bg text-accentSecondary p-4 hover:bg-brand hover:text-bg transition-all duration-300 ease-in-out"
+      >
         Previous Post
       </button>
-      <button className="rounded-md font-qs bg-bg text-accentSecondary p-4 hover:bg-brand hover:text-bg transition-all duration-300 ease-in-out">
+      <button
+        disabled={currentPage === totalPages}
+        onNext={onNext}
+        className="rounded-md font-qs bg-bg text-accentSecondary p-4 hover:bg-brand hover:text-bg transition-all duration-300 ease-in-out"
+      >
         Next Post
       </button>
     </div>
