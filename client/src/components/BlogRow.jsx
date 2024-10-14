54ea1a860ce96698592ef3f2330fe879ea28b516
德华 刘 <gavinprior5@gmail.com>
2024-10-14 09:57:56 +0000

    create blog card component

--- client/src/components/BlogRow.jsx
+++ client/src/components/BlogRow.jsx
@@ -1,64 +1,18 @@
 // BlogList.js
 import React from "react";
-import Card from "react-bootstrap/Card";
-import { NavLink } from "react-router-dom";
-import { MdLocationPin } from "react-icons/md";
 import Pagination from "./Pagination";
+import BlogCard from "./BlogCard";
 
-const BlogRow = ({
-  blogs,
-  currentPage,
-  totalPages,
-  onNext,
-  onPrevious,
-}) => {
+const BlogRow = ({ blogs, currentPage, totalPages, onNext, onPrevious }) => {
   return (
     <div className="w-3/4 flex flex-col items-center">
       <div>
         <div className="row">
-          {blogs.map((blog) => {
-            const { _id, title, heroImage, tags, destination } = blog;
-
-            return (
-              <div className="col-md-4 mb-4" key={_id}>
-                <Card className="h-100 text-text bg-bg">
-                  <Card.Img
-                    variant="top"
-                    src={heroImage}
-                    alt={title}
-                    className="object-cover h-[250px]"
-                  />
-                  <Card.Body className="d-flex flex-column">
-                    <Card.Title>{title}</Card.Title>
-                    <Card.Text>
-                      <div className="d-flex flex-wrap mb-2">
-                        {tags.map((tag, index) => (
-                          <div
-                            key={index}
-                            className="rounded bg-light border text-brand px-2 py-1 mr-2 mb-2"
-                          >
-                            {tag}
-                          </div>
-                        ))}
-                      </div>
-                      <div className="d-flex align-items-center mb-2">
-                        <MdLocationPin size={20} />{" "}
-                        <span className="ml-1">{destination}</span>
-                      </div>
-                    </Card.Text>
-                    <div className="mt-auto">
-                      <NavLink
-                        className="btn bg-brand text-bg"
-                        to={`/blog/${_id}`}
-                      >
-                        Read Blog
-                      </NavLink>
-                    </div>
-                  </Card.Body>
-                </Card>
-              </div>
-            );
-          })}
+          {Array.isArray(blogs) && blogs.length > 0 ? (
+            blogs.map((blog) => <BlogCard key={blog._id} blog={blog} />)
+          ) : (
+            <p>No blogs available</p>
+          )}
         </div>
         <Pagination
           currentPage={currentPage}
