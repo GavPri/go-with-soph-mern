0868a63c80138749113d6c763d22d6da1642648f
德华 刘 <gavinprior5@gmail.com>
2024-10-14 12:46:53 +0000

    add array to display mutiple cards

--- client/src/components/skeletonComponents/blogCardSkeleton.jsx
+++ client/src/components/skeletonComponents/blogCardSkeleton.jsx
@@ -3,33 +3,43 @@ import Skeleton from "react-loading-skeleton";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-const BlogCardSkeleton = () => {
+const BlogCardSkeleton = ({ cards }) => {
   return (
-    <div className="col-md-4 mb-4">
-      <Card className="h-100 text-text bg-bg">
-        <Skeleton height={250} />
-        <Card.Body className="d-flex flex-column">
-          <Card.Title>
-            <Skeleton />
-          </Card.Title>
-          <Card.Text>
-            <div className="d-flex flex-wrap mb-2">
-              <div className="rounded bg-light border text-brand px-2 py-1 mr-2 mb-2">
-                <Skeleton width={60} count={3} />
-              </div>
+    <div className="w-full flex flex-col items-center">
+      <div className="row w-3/4">
+        {" "}
+        {Array(cards)
+          .fill(0)
+          .map((_, i) => (
+            <div className="col-md-4 mb-4" key={i}>
+              <Card className="h-100 text-text bg-bg w-full">
+                <Skeleton height={250} />
+                <Card.Body className="d-flex flex-column">
+                  <Card.Title>
+                    <Skeleton />
+                  </Card.Title>
+                  <Card.Text>
+                    <div className="d-flex flex-wrap mb-2">
+                      <div className="rounded bg-light border text-brand px-2 py-1 mr-2 mb-2">
+                        <Skeleton width={60} count={3} />
+                      </div>
+                    </div>
+                    <div className="d-flex align-items-center mb-2">
+                      <Skeleton width={50} />
+                      <span className="ml-1">
+                        <Skeleton />
+                      </span>
+                    </div>
+                  </Card.Text>
+                  <div className="mt-auto">
+                    <Skeleton width={50} />
+                  </div>
+                </Card.Body>
+              </Card>
             </div>
-            <div className="d-flex align-items-center mb-2">
-              <Skeleton width={50} />
-              <span className="ml-1">
-                <Skeleton />
-              </span>
-            </div>
-          </Card.Text>
-          <div className="mt-auto">
-              <Skeleton width={50}/>
-          </div>
-        </Card.Body>
-      </Card>
+          ))}
+        ;
+      </div>
     </div>
   );
 };
