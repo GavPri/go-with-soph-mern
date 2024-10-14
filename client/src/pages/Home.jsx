1ac15708623ea4c5fe01e342094ce473cb8b2c5f
德华 刘 <gavinprior5@gmail.com>
2024-10-14 13:32:11 +0000

    add h2 tag to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="mt-20 min-h-[calc(100dvh-5rem)] bg-bg w-screen">
-      {/* Image wrapper */}
-      <div className="w-full">
-        <img
-          className="bg-[url('./images/nevadaFalls.JPG')] h-[calc(100dvh-5rem)] w-full object-cover"
-          src="./images/nevadaFalls.JPG"
-        />
+    <div className="mt-28 min-h-[calc(100dvh-5rem)] w-screen">
+      <div className="w-full h-fit flex flex-col items-center">
+        {/* ----- Header & arrows div ---- */}
+        <h2 className="font-qs text-text uppercase tracking-wide text-3xl text-left">
+          Disc<span className="text-brand italic">o</span>ver Popula
+          <span className="text-brand italic">r</span>
+          <br />
+          Destin<span className="text-brand italic">a</span>tions
+        </h2>
       </div>
     </div>
   );
