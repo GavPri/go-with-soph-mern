dc09c190d5485b360971eaa7ebf07c95d0cf55a7
德华 刘 <gavinprior5@gmail.com>
2024-10-14 12:48:47 +0000

    remove import of button

--- client/src/components/skeletonComponents/blogCardSkeleton.jsx
+++ client/src/components/skeletonComponents/blogCardSkeleton.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Skeleton from "react-loading-skeleton";
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 const BlogCardSkeleton = ({ cards }) => {
