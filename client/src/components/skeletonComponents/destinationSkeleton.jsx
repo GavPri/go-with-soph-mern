51b50a7354f60415bd06739a2c4cf564e47595aa
德华 刘 <gavinprior5@gmail.com>
2024-10-14 14:38:29 +0000

    add styles and array to skeleton

--- client/src/components/skeletonComponents/destinationSkeleton.jsx
+++ client/src/components/skeletonComponents/destinationSkeleton.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Skeleton from "react-loading-skeleton";
 
-const destinationSkeleton = () => {
+const DestinationSkeleton = ({ cards }) => {
   return (
-    <div>destinationSkeleton</div>
-  )
-}
+    <div className="w-full flex gap-2 flex-col justify-center items-center lg:flex-row lg:w-3/4 lg:justify-start">
+      {Array(cards)
+        .fill(0)
+        .map((_, i) => (
+          <Skeleton height={400} width={300} cards={cards} />
+        ))}
+    </div>
+  );
+};
 
-export default destinationSkeleton
+export default DestinationSkeleton;
