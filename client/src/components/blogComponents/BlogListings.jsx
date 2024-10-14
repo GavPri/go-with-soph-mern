c8424705aa5ad3920536e4d196e6cfc620923f8a
德华 刘 <gavinprior5@gmail.com>
2024-10-14 12:45:56 +0000

    add loading state to blog rows

--- client/src/components/blogComponents/BlogListings.jsx
+++ client/src/components/blogComponents/BlogListings.jsx
@@ -1,31 +1,38 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import BlogRow from "./BlogRow";
+import BlogCardSkeleton from "../skeletonComponents/blogCardSkeleton";
 
 const BlogListings = () => {
   // state for newest blog posts
   const [newestBlogs, setNewestBlogs] = useState([]);
   const [newestCurrentPage, setNewestCurrentPage] = useState(1);
   const [newestTotalPages, setNewestTotalPage] = useState(1);
+  const [newestIsLoading, setNewestIsLoading] = useState(false);
 
   //state for most popular blogs.
   const [mostLikedBlogs, setMostLikedBlogs] = useState([]);
   const [mostLikedCurrentPage, setMostLikedCurrentPage] = useState(1);
   const [mostLikedTotalPages, setMostLikedTotalPages] = useState(1);
+  const [mostLikedIsLoading, setMostLikedIsLoading] = useState(false);
 
   const getBlogListings = async () => {
+    setNewestIsLoading(true);
     const { data: newestData } = await axios.get(
       `/get-blogs?page=${newestCurrentPage}&limit=3&sortBy=newest`
     );
     console.log(newestData);
     setNewestBlogs(newestData.blogs);
     setNewestTotalPage(newestData.totalPages);
+    setNewestIsLoading(false);
 
+    setMostLikedIsLoading(true);
     const { data: LikesData } = await axios.get(
       `/get-blogs?page=${mostLikedCurrentPage}&limit=3&sortBy=mostLiked`
     );
     setMostLikedBlogs(LikesData.blogs);
     setMostLikedTotalPages(LikesData.totalPages);
+    setMostLikedIsLoading(false);
   };
 
   useEffect(() => {
@@ -60,23 +67,32 @@ const BlogListings = () => {
       <h2 className="my-6 text-text font-qs tracking-wide font-bold">
         Latest from <span className="text-brand">GoWithSoph</span>
       </h2>
-      <BlogRow
-        blogs={newestBlogs}
-        currentPage={newestCurrentPage}
-        onNext={handleNewestNextPage}
-        onPrevious={handleNewestPreviousPage}
-        totalPages={newestTotalPages}
-      />
+      {newestIsLoading ? (
+        <BlogCardSkeleton cards={3} />
+      ) : (
+        <BlogRow
+          blogs={newestBlogs}
+          currentPage={newestCurrentPage}
+          onNext={handleNewestNextPage}
+          onPrevious={handleNewestPreviousPage}
+          totalPages={newestTotalPages}
+        />
+      )}
+
       <h2 className="my-6 text-text font-qs tracking-wide font-bold">
         Most Liked from <span className="text-brand">GoWithSoph</span>
       </h2>
-      <BlogRow
-        blogs={mostLikedBlogs}
-        currentPage={mostLikedCurrentPage}
-        onNext={handleMostLikedNextPage}
-        onPrevious={handleMostLikedPreviousPage}
-        totalPages={mostLikedTotalPages}
-      />
+      {mostLikedIsLoading ? (
+        <BlogCardSkeleton cards={3} />
+      ) : (
+        <BlogRow
+          blogs={mostLikedBlogs}
+          currentPage={mostLikedCurrentPage}
+          onNext={handleMostLikedNextPage}
+          onPrevious={handleMostLikedPreviousPage}
+          totalPages={mostLikedTotalPages}
+        />
+      )}
     </div>
   );
 };
