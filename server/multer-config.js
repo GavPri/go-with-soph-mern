fa90ec8f9bd0d4a18eef8137f3e75ebb95b1282b
德华 刘 <gavinprior5@gmail.com>
2024-10-16 14:17:24 +0000

    create boilerplate fn for storage

--- server/multer-config.js
+++ server/multer-config.js
@@ -4,4 +4,8 @@ const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("./cloudinary-config");
 
+const storage = new CloudinaryStorage({
+    
+})
+
 module.exports = upload;
