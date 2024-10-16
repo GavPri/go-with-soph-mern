29586a493b0df7b81882327865cd6f10c503c7ad
德华 刘 <gavinprior5@gmail.com>
2024-10-16 14:19:02 +0000

    add params and cloudinary

--- server/multer-config.js
+++ server/multer-config.js
@@ -5,7 +5,11 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("./cloudinary-config");
 
 const storage = new CloudinaryStorage({
-    
-})
+  cloudinary: cloudinary,
+  params: {
+    folder: "blog_gallery",
+    allowed_formats: ["jpg", "png", "jpeg"],
+  },
+});
 
 module.exports = upload;
