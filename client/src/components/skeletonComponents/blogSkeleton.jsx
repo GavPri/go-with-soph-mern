f6ad19ccc0ee9256a3b4a6545eb957f62ae9a53e
德华 刘 <gavinprior5@gmail.com>
2024-10-21 17:31:35 +0000

    add loading skeleton to blog posts

--- client/src/components/skeletonComponents/blogSkeleton.jsx
+++ client/src/components/skeletonComponents/blogSkeleton.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Skeleton from "react-loading-skeleton";
 
 const BlogSkeleton = () => {
   return (
-    <div>blogSkeleton</div>
-  )
-}
+    <div className="w-full flex flex-col items-center">
+      {/* Hero skeleton */}
+      <div className="w-3/4 h-[350px] mb-8">
+        <Skeleton height={355} />
+      </div>
+      <div className="w-3/4 h-[350px] ">
+        <Skeleton count={6.5} />
+      </div>
+    </div>
+  );
+};
 
-export default BlogSkeleton
+export default BlogSkeleton;
