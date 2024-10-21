d17651d91e1f189fb141630d3a582effecb7e57f
德华 刘 <gavinprior5@gmail.com>
2024-10-21 12:00:11 +0000

    add margin top to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="w-3/4 h-64 bg-brand">Footer</div>;
+  return <div className="w-full h-64 bg-brand mt-auto">Footer</div>;
 };
 
 export default Footer;
