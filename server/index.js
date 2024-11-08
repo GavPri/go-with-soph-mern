2f950a93d4ef20541f087d440d9c8ff879dfd624
德华 刘 <gavinprior5@gmail.com>
2024-11-08 15:44:44 +0000

    for env varaible

--- server/index.js
+++ server/index.js
@@ -32,4 +32,8 @@ app.use("/api/", require("./routes/commentRoutes"));
 
 PORT = 8000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(
+    `Server is running on port ${PORT}`
+  )
+);
